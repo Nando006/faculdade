ee7f67e20e84de6e59e664c8f8011577a89ead20
Nando <FSNando.012@outlook.com>
2025-10-03 23:30:08 -0300

    Cria migrations para as tabelas ingredientes, pizza_ingredient, pedidos e pedido_pizza

---
+++ desenvolvimento_api_back-end/fase_2/sistema-pizzaria/database/migrations/2025_10_04_022652_create_pedido_pizza_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pedido_pizza', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('pedido_id')->constrained('pedidos')->cascadeOnDelete();
+            $table->foreignId('pizza_id')->constrained('pizzas')->cascadeOnDelete();
+
+            $table->integer('quantidade')->default(1);
+            $table->decimal('preco_unitario', 10,2);
+            $table->decimal('subtotal', 10,2);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pedido_pizza');
+    }
+};
